1c7988a6672be43b518508fbd7fa51e69028a269
Harry Marr <harry.marr@gmail.com>
2010-01-17 21:23:16 +0800

    Moved captcha from project to app

--- mumblr/entrytypes/captcha.py
+++ mumblr/entrytypes/captcha.py
@@ -9,15 +9,13 @@ class RecaptchaResponse(object):
         self.is_valid = is_valid
         self.error_code = error_code
 
-def displayhtml (public_key,
-                 use_ssl = False,
-                 error = None):
+def displayhtml(public_key, use_ssl = False, error = None):
     """Gets the HTML to display for reCAPTCHA
 
     public_key -- The public api key
     use_ssl -- Should the request be sent over ssl?
-    error -- An error message to display (from RecaptchaResponse.error_code)"""
-
+    error -- An error message to display (from RecaptchaResponse.error_code)
+    """
     error_param = ''
     if error:
         error_param = '&error=%s' % error
@@ -35,29 +33,30 @@ def displayhtml (public_key,
   <input type='hidden' name='recaptcha_response_field' value='manual_challenge' />
 </noscript>
 """ % {
-        'ApiServer' : server,
-        'PublicKey' : public_key,
-        'ErrorParam' : error_param,
+        'ApiServer': server,
+        'PublicKey': public_key,
+        'ErrorParam': error_param,
         }
 
 
-def submit (recaptcha_challenge_field,
-            recaptcha_response_field,
-            private_key,
-            remoteip):
+def submit(recaptcha_challenge_field, recaptcha_response_field, private_key,
+           remoteip):
     """
     Submits a reCAPTCHA request for verification. Returns RecaptchaResponse
     for the request
 
-    recaptcha_challenge_field -- The value of recaptcha_challenge_field from the form
-    recaptcha_response_field -- The value of recaptcha_response_field from the form
+    recaptcha_challenge_field -- The value of recaptcha_challenge_field from
+    the form
+    recaptcha_response_field -- The value of recaptcha_response_field from the
+    form
     private_key -- your reCAPTCHA private key
     remoteip -- the user's ip address
     """
 
     if not (recaptcha_response_field and recaptcha_challenge_field and
-            len (recaptcha_response_field) and len (recaptcha_challenge_field)):
-        return RecaptchaResponse (is_valid = False, error_code = 'incorrect-captcha-sol')
+            len(recaptcha_response_field) and len(recaptcha_challenge_field)):
+        return RecaptchaResponse(is_valid=False, 
+                                 error_code='incorrect-captcha-sol')
     
 
     def encode_if_necessary(s):
@@ -65,32 +64,30 @@ def submit (recaptcha_challenge_field,
             return s.encode('utf-8')
         return s
 
-    params = urllib.urlencode ({
-            'privatekey': encode_if_necessary(private_key),
-            'remoteip' :  encode_if_necessary(remoteip),
-            'challenge':  encode_if_necessary(recaptcha_challenge_field),
-            'response' :  encode_if_necessary(recaptcha_response_field),
-            })
+    params = urllib.urlencode({
+        'privatekey': encode_if_necessary(private_key),
+        'remoteip':  encode_if_necessary(remoteip),
+        'challenge':  encode_if_necessary(recaptcha_challenge_field),
+        'response':  encode_if_necessary(recaptcha_response_field),
+    })
 
-    request = urllib2.Request (
+    request = urllib2.Request(
         url = "http://%s/verify" % VERIFY_SERVER,
         data = params,
         headers = {
             "Content-type": "application/x-www-form-urlencoded",
             "User-agent": "reCAPTCHA Python"
-            }
-        )
+        }
+    )
     
-    httpresp = urllib2.urlopen (request)
+    httpresp = urllib2.urlopen(request)
 
-    return_values = httpresp.read ().splitlines ();
+    return_values = httpresp.read().splitlines();
     httpresp.close();
 
     return_code = return_values [0]
 
-    if (return_code == "true"):
-        return RecaptchaResponse (is_valid=True)
+    if return_code == "true":
+        return RecaptchaResponse(is_valid=True)
     else:
-        return RecaptchaResponse (is_valid=False, error_code = return_values [1])
-
-
+        return RecaptchaResponse(is_valid=False, error_code=return_values[1])
--- mumblr/entrytypes/fields.py
+++ mumblr/entrytypes/fields.py
@@ -1,10 +1,23 @@
 from django.conf import settings
 from django import forms
+from django.utils.safestring import mark_safe
 from django.utils.encoding import smart_unicode
 from django.utils.translation import ugettext_lazy as _
 
-from widgets import ReCaptcha
 import captcha
+
+
+class ReCaptcha(forms.widgets.Widget):
+    """Renders the proper ReCaptcha widget
+    """
+    def render(self, name, value, attrs=None):
+        html = captcha.displayhtml(settings.RECAPTCHA_PUBLIC_KEY)
+        return mark_safe(u'%s' % html)
+
+    def value_from_datadict(self, data, files, name):
+        return [data.get('recaptcha_challenge_field', None), 
+                data.get('recaptcha_response_field', None)]
+
 
 class ReCaptchaField(forms.CharField):
     """Provides ReCaptcha functionality using ReCaptcha python client
