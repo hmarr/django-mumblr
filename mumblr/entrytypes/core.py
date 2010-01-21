0f80e95f3734ef3da08e649fdcda165606ad3bb7
Steve Challis <steve.challis@gmail.com>
2010-01-21 14:04:29 +0000

    New video post type with automatic YouTube embed

--- mumblr/entrytypes/core.py
+++ mumblr/entrytypes/core.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 
 from mongoengine import *
@@ -72,6 +73,45 @@ class ImageEntry(EntryType):
         description = forms.CharField(widget=forms.Textarea, required=False)
 
 
+class VideoEntry(EntryType):
+    """An video-based entry - will try to embed the video if it is of
+    and known type e.g. YouTube
+    """
+    video_url = StringField(required=True)
+    description = StringField()
+
+    type = 'Video'
+
+    def rendered_content(self):
+        video_url = self.video_url
+        pattern = r'youtube\.com\/watch\?v=([A-Za-z0-9._%-]+)[&\w;=\+_\-]*'
+        id = re.findall(pattern, video_url)
+        flash_url = 'http://www.youtube.com/v/%s&fs=1&rel=&hd=10&showinfo=0'
+        embed = '<object width="620" height="360">' \
+                '<param name="movie" value="%s"></param>' \
+                '<param name="allowFullScreen" value="true"></param>' \
+                '<param name="allowscriptaccess" value="always"></param>' \
+                '<embed src="%s"' \
+                'type="application/x-shockwave-flash" ' \
+                'allowscriptaccess="always" allowfullscreen="true" ' \
+                'width="620" height="360"></embed></object>'
+        if id:
+            # We found at least one ID so put it in a url
+            url = flash_url % id[0]
+            # Put url in embedding template
+            html = embed % (url, url)
+        else:
+            html = 'Video: <a href="video_url">%s</a>' % video_url
+        if self.description:
+            html += markup(self.description)
+        return html
+
+    class AdminForm(EntryType.AdminForm):
+        video_url = forms.URLField()
+        description = forms.CharField(widget=forms.Textarea, required=False)
+
+
 EntryType.register(TextEntry)
 EntryType.register(LinkEntry)
 EntryType.register(ImageEntry)
+EntryType.register(VideoEntry)
